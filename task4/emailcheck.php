04700c2b059a18a15ebc0d523999f6cf90825287
Najmus Saquib <152478433+0NJS0@users.noreply.github.com>
2024-11-18 13:41:44 +0600

    unfinished

--- task4/emailcheck.php
+++ task4/emailcheck.php
@@ -1,23 +1,45 @@
 <?php 
 
     if(isset($_POST['submit'])){
-        $username = $_POST['username'];
-
+        $username = $_POST['email'];
+        $atfound= false;
+        $dotfound= false;
+        $flag = 0;
+        for($i=0; $i <strlen($username);$i++)
+        {
+            $char=$username[$i];
+            if($char=='@')
+            {
+                if($atfound)
+                {
+                    $flag=1;
+                }
+                $atfound=true;
+            }
+            elseif($char=='.')
+            {
+                if($dotfound)
+                {
+                    $flag=1;
+                }
+                $dotfound=true;
+            }
+        }
         if($username == null){
             echo "Null Entry";
         }
-        elseif(strlen($username)<2){
-            echo "Entry is less than 2";
-        }
-        elseif(i){
-            echo "Entry is less than 2";
+
+        elseif($flag==1){
+            echo "Invalid Email";
         }
     else{
-        header('location: page2.html');
+        header('location: page3.html');
     }
+}
+
     else{
         //echo "invalid request!";
-        header('location: page 1.html');
+        header('location: page 2.html');
     }
 
 
--- task4/gendercheck.php
+++ task4/gendercheck.php
@@ -6,12 +6,10 @@
         if($username == null){
             echo "Null Entry";
         }
-        elseif(strlen($username)<2){
-            echo "Entry is less than 2";
-        }
     else{
         header('location: page4.html');
     }
+}
     else{
         //echo "invalid request!";
         header('location: page3.html');
